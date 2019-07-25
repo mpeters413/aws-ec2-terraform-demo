11e013faddfe908709087454f506edac2738594a
Matthew Peters <mpeters413@gmail.com>
2019-07-25 12:20:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,7 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-
-  tags {
-    Name = "${var.name}"
-  }
+  
 }
 
 
