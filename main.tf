1e4c444f9155aa9fca4b1349c60d790e8b7e6a29
Matthew Peters <mpeters413@gmail.com>
2019-10-16 12:12:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,11 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-  tags  = "${var.name}"
+  tags = 
+    map(
+      "Name", "matts-sever",
+      "Role", "server"
+    )
 }
 
 
