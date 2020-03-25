d95cb4039bc2689a6522f64039f05ec86772cff2
Matthew Peters <mpeters413@gmail.com>
2020-03-25 18:19:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ terraform {
 
 provider "aws" {
   region = "${var.aws_region}"
- 
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_instance" "ubuntu" {
