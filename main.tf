89bc4f6ffb279fc8c88874677735abe221fa09ff
Matthew Peters <mpeters413@gmail.com>
2020-04-24 13:45:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-  key_name = "mattpeters
+  key_name = "mattpeters"
 }
 
 
