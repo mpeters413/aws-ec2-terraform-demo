223673d360ad6155ab5110ba16240ea702f7952e
mpeters413 <mpeters413@gmail.com>
2020-04-24 13:52:55 -0400

    update instance size

--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.medium"
+  default = "t2.micro"
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "matts_demo_instance"
+  default = "mattspeters"
 }
 
 ////////////////////
