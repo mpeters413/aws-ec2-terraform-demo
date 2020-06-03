ad965776d67f33dc1a3fd10a44c92bc9d40ae0d1
Matthew Peters <mpeters413@gmail.com>
2020-06-03 17:51:49 -0400

    Create variables.tf

---
+++ compute/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-2e1ef954"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.small"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "mattspeters"
+}
+
+////////////////////
+
+variable "subnets" {
+  description = "subnets"
+  default = "subnet-0650c811e6d317a8b"
+}
+
+variable "health_check" {
+  default = "HTTP:8200/v1/sys/health?standbyok=true&perfstandbyok=true"
+  description = "Health check for load balancer"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+  description = "aws access key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+  description = "aws access key"
+}
