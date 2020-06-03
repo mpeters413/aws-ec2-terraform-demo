98f9c430834b5cdde1a210878bdcdfb67b95e594
Matthew Peters <mpeters413@gmail.com>
2020-06-03 17:04:52 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "vault_addr" {
-  description = "vault address"
-  default = "http://mattspeters-benchmark-vault-elb-1366644832.us-east-1.elb.amazonaws.com:8200"
-  }
-
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
   default = "ami-2e1ef954"
