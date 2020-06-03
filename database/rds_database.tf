2b1c6fa51e11b4344db69e3bf96c25300bf006ee
Matthew Peters <mpeters413@gmail.com>
2020-06-03 18:29:55 -0400

    Update rds_database.tf

--- database/rds_database.tf
+++ database/rds_database.tf
@@ -8,4 +8,6 @@ resource "aws_db_instance" "default" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+  
 }
