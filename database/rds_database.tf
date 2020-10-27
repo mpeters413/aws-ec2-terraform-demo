400a1f9cf0633f904626eb4f0203149d06db19a3
mpeters413 <mpeters413@gmail.com>
2020-10-27 14:53:35 -0400

    updating DB size

--- database/rds_database.tf
+++ database/rds_database.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.medium"
+  instance_class       = "db.t2.small"
   name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
