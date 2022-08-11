1b52cf41ad45f76a5aa76f37f1235fd11190cdab
Matthew Peters <mpeters413@gmail.com>
2022-08-11 10:23:20 -0400

    Update rds_database.tf

--- database/rds_database.tf
+++ database/rds_database.tf
@@ -1,4 +1,4 @@
-resource "aws_db_instance" "default" {
+/* resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
@@ -11,3 +11,5 @@ resource "aws_db_instance" "default" {
   skip_final_snapshot  = "true"
   
 }
+
+*/
