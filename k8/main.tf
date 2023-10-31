b4e897f55dc5ab802008de4490d51c8ef1bbc180
Matthew Peters <mpeters413@gmail.com>
2023-10-31 13:52:08 -0400

    Update main.tf

--- k8/main.tf
+++ k8/main.tf
@@ -19,9 +19,9 @@ data "vault_aws_access_credentials" "aws_creds" {
 
 
 provider "aws" {
-  region = "${var.aws_region}"
-  access_key = "${data.vault_aws_access_credentials.aws_creds.access_key}"
-  secret_key = "${data.vault_aws_access_credentials.aws_creds.secret_key}"
+  region = var.aws_region
+  access_key = {data.vault_aws_access_credentials.aws_creds.access_key
+  secret_key = {data.vault_aws_access_credentials.aws_creds.secret_key}
 }
 
 
