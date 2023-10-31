ee8398d7195e7a3997c7332d599bce0f900d096e
Matthew Peters <mpeters413@gmail.com>
2023-10-31 13:30:30 -0400

    Update variables.tf

--- k8/variables.tf
+++ k8/variables.tf
@@ -5,3 +5,13 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+  description = "aws access key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+  description = "aws access key"
+}
